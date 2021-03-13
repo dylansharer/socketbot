dd4ebfc562a6954cc105be6d08b4d789cf4fe1c7
Dylan Sharer <dylansharer@Dylans-MacBook-Air.local>
2021-03-13 11:38:49 -0700

    discord bot revamp

--- commands/socials.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (_client, message, _args) => {
-    message.channel.send(`https://opensea.io/collection/sockets`);
-    message.channel.send(`https://twitter.com/SocketsNFT`);
-    message.channel.send(`https://www.reddit.com/user/SocketsNFT`);
-}
-  
-module.exports.help = {
-    name: 'socials'
-}
